45c04c5e17cdf3696376ba11a58319759d7c3331
VyDink <vy.dinh.2929@gmail.com>
2025-06-30 16:38:49 +0700

    Reponsive for mobile and add a Marquee animation to show animated cards

--- src/layout/LiveAcution.tsx
+++ src/layout/LiveAcution.tsx
@@ -2,25 +2,28 @@ import ArrowLeft from "../assets/Icons/ArrowLeft";
 import ArrowRight from "../assets/Icons/ArrowRight";
 import type { CardAcutionProps } from "../types/CardAcutionProps";
 import CardAcution from "../components/Card";
+import Marquee from "react-fast-marquee";
 
 const LiveAcution = ({ cardInfo }: CardAcutionProps) => {
   return (
     <section className="md:max-w-[1360px]  m-auto pt-[150px]">
       <div className="text-center">
-        <h1 className="text-5xl">Live Acution</h1>
+        <h1 className="text-2xl md:text-5xl">Live Acution</h1>
         <p className="text-base pt-5">
           The largest and unique Super rare NFT marketplace <br /> For crypto-collectibles
         </p>
       </div>
-      <div className="mt-12 relative">
-        <button className="absolute left-0 top-1/2 z-10  ">
+      <div className="mt-12 relative ">
+        <button className="absolute hidden md:block left-0 top-1/2 z-10  ">
           <ArrowLeft />
         </button>
-        <div className="flex gap-7 justify-center">
-          <CardAcution cardInfo={cardInfo} />
+        <div className="flex justify-center">
+          <Marquee speed={100} pauseOnClick className="overflow-hidden">
+            <CardAcution cardInfo={cardInfo} />
+          </Marquee>
         </div>
 
-        <button className="absolute right-0 top-1/2 z-10 ">
+        <button className="absolute hidden md:block -right-5 top-1/2 z-10 ">
           <ArrowRight />
         </button>
       </div>
