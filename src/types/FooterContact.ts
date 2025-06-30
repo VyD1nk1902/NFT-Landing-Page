c2124b105e70f2a64f0cbb82907267321ebb68ba
VyDink <vy.dinh.2929@gmail.com>
2025-06-30 16:36:45 +0700

    FooterContact data types

---
+++ src/types/FooterContact.ts
@@ -0,0 +1,11 @@
+interface FooterContact {
+  title: string;
+  link1: string;
+  link2: string;
+  link3: string;
+  link4: string;
+}
+
+export interface FooterContactProps {
+  contact: FooterContact[];
+}
